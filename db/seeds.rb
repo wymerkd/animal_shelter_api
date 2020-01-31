5fe9ee3852635caf1feb77ea9f8dd5d86fb6e348
wymerkd <wymerkd@gmail.com>
2020-01-31 14:43:15 -0800

    setup seed structure.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_animals
+  end
+
+  def generate_animals
+    5.times do |i|
+      animal = Animal.create!(
+        name: Faker::Book.author,
+        breed: Faker::Book.author,
+        color: Faker::Book.author,
+        age: Faker::Book.author,
+        weight: Faker::Book.author,
+        description: Faker::Movie.quote
+      )
+      puts "Animal #{i}: Name: #{animal.name} Breed: #{animal.breed} Color: #{animal.color} Age: #{animal.age} Weight: #{animal.weight} Description: #{animal.description}"
+    end
+  end
+end
+
+Seed.begin
