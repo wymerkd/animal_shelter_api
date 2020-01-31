6ad0b39c02b1aa2575202b1637456977dd2b1557
wymerkd <wymerkd@gmail.com>
2020-01-31 15:00:29 -0800

    add fakers to seed database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,12 @@ class Seed
   def generate_animals
     5.times do |i|
       animal = Animal.create!(
-        name: Faker::Book.author,
-        breed: Faker::Book.author,
+        name: Faker::Creature::Dog.name,
+        breed: Faker::Creature::Dog.breed,
         color: Faker::Book.author,
-        age: Faker::Book.author,
-        weight: Faker::Book.author,
-        description: Faker::Movie.quote
+        age: Faker::Creature::Dog.age,
+        weight: Faker::Measurement.weight,
+        description: Faker::Restaurant.type
       )
       puts "Animal #{i}: Name: #{animal.name} Breed: #{animal.breed} Color: #{animal.color} Age: #{animal.age} Weight: #{animal.weight} Description: #{animal.description}"
     end
